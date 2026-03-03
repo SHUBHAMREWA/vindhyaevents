67f69b1cb2de574960dd3cb7b59194946d4a29df
shubham kushwaha <shubhamrewamp17@gmail.com>
2026-03-03 18:15:11 +0530

    sitemap and robot.js file added for seo

---
+++ src/app/robots.js
@@ -0,0 +1,11 @@
+export default function robots() {
+
+    return {
+
+        rules: {
+            userAgent: "*",
+            allow: "/"
+        },
+        siteMap: 'https://vindhyaevent.vercel.app/sitemap.xml'
+    }
+}
---
+++ src/app/sitemap.js
@@ -0,0 +1,22 @@
+export default function sitemap() {
+   
+ return [
+    {
+      url: 'https://vindhyaevent.vercel.app',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://vindhyaevent.vercel.app/gallery',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://vindhyaevent.vercel.app/about',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://vindhyaevent.vercel.app/blog',
+      lastModified: new Date(),
+    },
+  ]
+
+}
